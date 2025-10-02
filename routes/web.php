16ece6580b7ce2f84be4e2cef415186ee01d460e
fernando-FTD <84219686+fernando-FTD@users.noreply.github.com>
2025-10-02 21:08:35 +0700

    Selesai Langkah Praktikum Modul 4

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfilController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
